abc0ade8ee60023f989dc29d894aa4185c4fb908
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-08-07 08:14:55 -0300

    Update transaction_list.dart

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     //Componente pai para delimitar a area do ListView
     return Container(
-      height: MediaQuery.of(context).size.height * 0.6,
+      height: MediaQuery.of(context).size.height * 0.50,
       child: _transactions.isEmpty
           ? Column(
               children: [
